dbbfaee4e160c1c0ae05b1c2dfaec69b1c41eb02
aray <Shivansh@#155>
2024-07-17 13:47:18 +0530

    updated message

--- target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-insert into `users` (`id`, `username`, `email`) values
-(1, 'bob.smith', 'bob.smith@email.com'),
-(2, 'jake.potter', 'jake.potter@email.com'),
-(3, 'sarah.thompson', 'sarah.thompson@email.com');
